f6cf884863854085df4307d311763c26d67dce28
Joowon Ryoo <joowonryoo@gmail.com>
2016-01-26 19:15:16 +0900

    Add Help Option

--- src/com/hipercube/compressor/cli/CompressorClParser.java
+++ src/com/hipercube/compressor/cli/CompressorClParser.java
@@ -6,4 +6,9 @@ import org.apache.commons.cli.*;
  * Created by HIPERCUBE on 1/26/16.
  */
 public class CompressorClParser extends DefaultParser {
+    private final Options options = new Options();
+
+    public CompressorClParser() {
+        options.addOption("l", "location", true, )
+    }
 }
